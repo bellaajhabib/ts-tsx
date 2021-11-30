9233a6fb99c668aacca5cb727c286fbb105bb293
Habib <habib.bellaaj@studyrama.com>
2021-11-30 17:09:45 +0100

    work with Tuples

--- app.ts
+++ app.ts
@@ -1,6 +1,20 @@
-const person:{ name:string,age: } ={
+const person:{
+    name:string,
+    age:number,
+    hobbies:string[],
+    role: [number,string]
+} ={
     name:'Habib',
-    age:36
-
+    age:36,
+    hobbies:['spots','cooking'],
+    role:[10,'admin']
 }
-console.log(person.name)
+
+let favoriteActivities:string;
+favoriteActivities ='hiking';
+person.role.push('users');
+person.hobbies.push(favoriteActivities);
+for (const hobby of person.role){
+    console.log(hobby);
+}
+
